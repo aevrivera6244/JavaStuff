076c74fd3460409bb347a7904c45ceb6bed315f1
aevrivera6244 <145944226+aevrivera6244@users.noreply.github.com>
2024-02-09 12:27:06 +0800

    Stacks.java

--- Stacks and Queues/Stacks.java
+++ Stacks and Queues/Stacks.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Stacks2 {
+public class Stacks {
 
 	public static void main(String[] args) {
 		
@@ -10,15 +10,72 @@ public class Stacks2 {
 		Scanner scanner2 = new Scanner(System.in);
 		int top = -1;
 		
-		System.out.print("Enter value of stack elements: ");
+		System.out.print("Enter value of stack elements(0 - infinite): ");
 	    int ElemNum = scanner.nextInt();
+	    int elemcount = -1;
 	    
 	    while(true)
 	    {
-	    	System.out.println("\n(1) Push \n(2) Pop \n(3) Exit");
+	    	if(ElemNum == 0)
+	    	{
+	    		System.out.println("\n(1) Push \n(2) Pop \n(3) Display Stack \n(4) Exit");
+			    System.out.print("Please select an operation: ");
+			    int Operation = scanner.nextInt();
+			    if(Operation == 1) // Push (infinite)
+		    	{
+		    		System.out.println("\nPush() Selected.");
+					if(stack.empty() == true || stack.empty() == false)
+					{
+						elemcount++;
+						System.out.print("Enter Element[" + elemcount + "]: ");
+				    	String Elem = scanner2.nextLine();
+				    	stack.add(Elem);
+				    	System.out.println("Stack: " + stack);
+				    	continue;
+					}
+					break;
+		    	}
+			    else if(Operation == 2) // Pop
+		    	{
+		    		System.out.println("\nPop() Selected.");
+					if(stack.empty() == false)
+					{
+						System.out.print("Popped Element[" + elemcount + "]: " + stack.pop()+ "\n");
+						elemcount--;
+				    	System.out.println("Stack: " + stack);
+				    	continue;
+					}
+					else if(elemcount == -1)
+					{
+						System.out.println("Stack is Empty");
+						continue;
+					}
+		    	}
+				else if(Operation == 3) // Display Stack
+			    {
+			    	System.out.println("Stack: " + stack);
+			    	continue;
+			    }
+			    else if(Operation == 4) // Exit
+			    {
+			    	System.out.println("Stack: " + stack);
+			    	break;
+			    }
+			    else
+		    	{
+		    		System.out.println("Error, please try again.");
+		    		continue;
+		    	}
+				break;
+		    		
+		    }
+	    	
+	    	// If ElemNum not is NOT equal to 0
+	    	
+	    	System.out.println("\n(1) Push \n(2) Pop \n(3) Display Stack \n(4) Exit");
 		    System.out.print("Please select an operation: ");
 		    int Operation = scanner.nextInt();
-	    	if(Operation == 1)
+	    	if(Operation == 1) // Push
 	    	{
 	    		System.out.println("\nPush() Selected.");
 	    		top++;
@@ -38,7 +95,7 @@ public class Stacks2 {
 				}
 				break;
 	    	}
-	    	else if(Operation == 2)
+	    	else if(Operation == 2) // Pop
 	    	{
 	    		System.out.println("\nPop() Selected.");
 				if(stack.empty() == false)
@@ -56,7 +113,12 @@ public class Stacks2 {
 				break;
 	    		
 	    	}
-	    	else if(Operation == 3)
+	    	else if(Operation == 3) // Display Stack
+	    	{
+	    		System.out.println("Stack: " + stack);
+	    		continue;
+	    	}
+	    	else if(Operation == 4) // Exit
 	    	{
 	    		System.out.println("Stack: " + stack);
 	    		break;
